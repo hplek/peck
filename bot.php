24a6ff5d524e42506a0524737afb0cedcac7175f
hplek <32577718+hplek@users.noreply.github.com>
2017-10-13 12:13:22 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,2 +1,89 @@
 <?php
-echo "I am a bot";
+$access_token = '0vAGpxZG7+30kW017m20c4MbGes89RLuH9TgE2sEmT/6Boz4ZEyUUsdK63kae3MEeMe1HEnUoNj6F2o6e/DtdoYrt0Lkm4mYESRfyw9/LJdpDcNvxfAms4DUCGN3iEWO+3W4LAFbFptmDe+r2dofjgdB04t89/1O/w1cDnyilFU=';
+$roomToken = 'U3282f479864b26d1efd54b16de07a8dc';
+// Get POST body content
+$content = file_get_contents('php://input');
+// Parse JSON
+$events = json_decode($content, true);
+// Validate parsed JSON data
+if (!is_null($events['events'])) {
+	// Loop through each event
+	foreach ($events['events'] as $event) {
+		// Reply only when message sent is in 'text' format
+		$join = 0;
+		if ($event['type'] == 'join'){
+			$join = 1;
+		}
+		if ( ($event['type'] == 'message' && $event['message']['type'] == 'text' ) || $join == 1) {
+			// Get text sent
+			
+			$question = [
+				0 => [
+					'keywords' => 'ดีจ้า',
+					'ans' => 'สวัสดีครับ'
+				],
+				1 => [
+					'keywords' => 'แนะนำ',
+					'ans' => 'ผมเป็นบอทครับ'
+				],
+				2 => [
+					'keywords' => 'เหงา',
+					'ans' => 'เหงา ๆ ก็คุยกับผมได้นะ'
+				]
+								
+			];
+			
+			$found = 0;
+			if($join == 0){
+				foreach ( $question as $row ){
+					if( strpos($event['message']['text'],$row['keywords']) !== false ){
+						$text = $row['ans'];
+						$found = 1;
+						break;
+					}
+				}
+			}else{
+				$text = "ทุกๆคนที่เข้ามาใหม่ เมื่อเข้ามาแล้วให้ทำดังนี้นะคะ \n 1.) แนะนำตัวเอง \n 2.) ประทับใจอะไรใน bnk48 \n แล้วอดใจรอซักครู่นะคะ";	
+			}
+			
+			if($found !== 0 || $join == 1){
+				//$text = 'ช้อนไม่เข้าใจ ช้อน SO VERY กระจอก';
+			
+				//$text = $event['message']['text'];
+					// Get replyToken
+				$replyToken = $event['replyToken'];
+
+				// Build message to reply back
+				$messages = [
+					'type' => 'text',
+					'text' => $text
+				];
+
+				// Make a POST Request to Messaging API to reply to sender
+				$url = 'https://api.line.me/v2/bot/message/reply';
+				$data = [
+					'replyToken' => $replyToken,
+					'messages' => [$messages],
+				];
+				$post = json_encode($data);
+				$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+				
+				$ch = curl_init($url);
+				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+				curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+				curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+				curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+				$result = curl_exec($ch);
+				curl_close($ch);
+				echo $result . "\r\n";
+
+			}
+		}
+	}
+	
+}
+
+echo "OK";
+
+?>
