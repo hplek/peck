37ca235932627eeddb210fc6728ecf2591900e16
hplek <32577718+hplek@users.noreply.github.com>
2017-10-14 00:52:11 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -7,7 +7,7 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
     </head>
 <body>
-<nav class="navbar navbar-toggleable-md navbar-inverse" style="background-color: sky;">
+<nav class="navbar navbar-toggleable-md navbar-inverse" style="background-color: cyan;">
     <a class="navbar-brand pull-middle" href="#"><strong>Manage Bot Data</strong></a>
 </nav>
 
